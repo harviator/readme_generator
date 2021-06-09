6667c204971976b7df1ca1ac5b9dae087a2f15c1
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-08 20:04:24 -0400

    Added basic file package that came with homework

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+/*
+Notes:
+1. A command-line application that accepts user input
+2. Prompts for:
+    -Title of my project
+    -Sections:
+        -Description
+        -Table of Contents:
+            -Links to the corresponding section
+        -Installation
+        -Usage
+        -License(from a list of options):
+            -Badge for that licence is added near the top of the readme
+            -Notice explaining which license the application is covered under
+        -Contributing Guidelines
+        -Test Instructions
+        -Questions:
+            -GitHub user name and profile link
+            -email with instructions how to reach me?
+*/
