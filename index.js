7b5a577b1230bed7d9cd9f7deff74c952b0f05dd
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-10 16:37:20 -0400

    Almost have the questions array finished and adding the responses to the genereateMarkdown.js file

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of your application?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Provide a brief description of your application",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "How is your application installed?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "How is your application used?",
+        name: "usage"
+    },
+    {
+        type: "List",
+        //choices
+        message: "Please select a license for your application",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What are the guidelines for contributers to your application?",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "How can someone test your application?",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "githubUsername"
+    },
+    {
+        type: "input",
+        message: "What is the link to your GitHub profile?",
+        name: "githubProfile"
+    },
+    {
+        type: "input",
+        message: "What are some instructions for people wanting to contact you by email?",
+        name: "contactInstructions"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        const generateMarkdown = require('./utils/generateMarkdown.js');
+        
+        const returnData = generateMarkdown(response);
+
+        //writeToFile()
+    })
+}
 
 // Function call to initialize app
 init();
