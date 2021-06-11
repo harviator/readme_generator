e5ccf3b3c21b9fd9b14eedb8f2f025b0079544a8
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-10 20:32:55 -0400

    finished the the renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,30 @@
+const license = response.license;
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "Apache":
+      return `![Apache Badge](https://img.shields.io/badge/License-Apache-blue)`;
+      break;
+    case "GNU":
+      return `![GNU Badge](https://img.shields.io/badge/License-GNU-red)`;
+      break;
+    case "ISC":
+      return `![ISC Badge](https://img.shields.io/badge/License-ISC-orange)`;
+      break;
+    case "MIT":
+      return `![MIT Badge](https://img.shields.io/badge/License-MIT-green)`;
+      break;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +33,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
   
-  return `
+  return `${renderLicenseBadge}
   # ${response.title}
 
   ## Descrition 
